d54ebabac72ca6c4033c0240fb9199826badedbc
Bruno Barange <bruno.akun@gmail.com>
2022-10-18 16:13:18 +0200

    04_am_table

--- README.md
+++ README.md
@@ -1,27 +1,16 @@
 # 04AmTable
+https://amoelcodigo.com/angular-material-table-excel/?unapproved=193&moderation-hash=24baa04777b17abdce689a195b302444#comment-193
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0.
+Exportar Tablas de Angular Material a Excel, Json y Archivos de texto con mat-table-exporter
 
-## Development server
+## Install ng-material y exporter
+ng add @angular/material
+npm install mat-table-exporter
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+## Modelo de datos
+ng g i models/tabla
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,48 @@
+import { ITabla } from './../../models/tabla';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
+})
+export class TableComponent implements OnInit {
+
+  constructor() { }
+
+  
+  @ViewChild(MatSort) sort:any ;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  elementData: ITabla[] = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  ];
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(this.elementData);
+
+  ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+}
